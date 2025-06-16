435d9e97680691bf3ab4c2553c046651e8b841a7
Oleg Hoffmann <oleg.hoffmann239@gmail.com>
2025-06-16 10:35:37 +0200

    Added C++

---
+++ Source/UE5_DefaultTemplate/UE5_DefaultTemplate.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE5_DefaultTemplate.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_DefaultTemplate, "UE5_DefaultTemplate" );
